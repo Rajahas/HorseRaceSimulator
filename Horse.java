563baa96608a9b426f79589e77437ea431cae2ed
Rajahas <rajahas2006@gmail.com>
2025-04-22 20:59:50 +0100

    Update Horse.java

--- Horse.java
+++ Horse.java
@@ -13,19 +13,37 @@ public class Horse
   private double horseConfidence;
   private boolean fallen;
   private int lane;
+  private int wins;
+  private int races;
     
   //Constructor of class Horse
   /**
    * Constructor for objects of class Horse
    */
+  // Added methods to overload
+  public Horse(char horseSymbol, String horseName, double horseConfidence, int lane, int races, int wins)
+  {
+    this.horseSymbol = horseSymbol;
+    this.horseName = horseName;
+    this.setConfidence(horseConfidence);
+    this.horseDistance = 0;
+    this.fallen = false;
+    this.lane = lane;
+    this.wins = races;
+    this.races = wins;
+  }
+
+  // Default constructor
   public Horse(char horseSymbol, String horseName, double horseConfidence, int lane)
   {
     this.horseSymbol = horseSymbol;
     this.horseName = horseName;
-    this.horseConfidence = horseConfidence;
+    this.setConfidence(horseConfidence);
     this.horseDistance = 0;
     this.fallen = false;
     this.lane = lane;
+    this.wins = 0;
+    this.races = 0;
   }
   
   //Other methods of class Horse
@@ -69,16 +87,6 @@ public class Horse
     this.horseDistance += 1; 
   }
 
-  public int getLane()
-  {
-    return this.lane;
-  }
-
-  public void setLane(int n)
-  {
-    this.lane = n;
-  }
-
   public void setConfidence(double newConfidence)
   {
     if (newConfidence > 1.0)
@@ -98,5 +106,51 @@ public class Horse
   public void setSymbol(char newSymbol)
   {
     this.horseSymbol = newSymbol;
+  }
+
+  // Added methods
+  public int getLane()
+  {
+    return this.lane;
+  }
+
+  public void setLane(int n)
+  {
+    this.lane = n;
+  }
+
+  public void increaseConfidence()
+  {
+    this.horseConfidence = Math.min(1.0, this.horseConfidence + 0.05);
+  }
+
+  public void decreaseConfidence()
+  {
+    this.horseConfidence = Math.max(0.0, this.horseConfidence - 0.05);
+  }
+
+  public void resetFallen()
+  {
+    this.fallen = false;
+  }
+
+  public void setWins(int n)
+  {
+    this.wins = n;
+  }
+
+  public int getWins()
+  {
+    return this.wins;
+  }
+
+  public void setRaces(int n)
+  {
+    this.races = n;
+  }
+
+  public int getRaces()
+  {
+    return this.races;
   }
 }
